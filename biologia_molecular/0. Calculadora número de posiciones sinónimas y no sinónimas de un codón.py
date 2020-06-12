eb9cd5fe28f4b71532483df3ddbf43d06a187894
miguelocana <56848883+miguelocana@users.noreply.github.com>
2020-06-12 13:34:33 +0200

    Add files via upload

---
+++ biologia_molecular/0. Calculadora número de posiciones sinónimas y no sinónimas de un codón.py
@@ -0,0 +1,122 @@
+from time import sleep
+def translate(seq):        
+    table = { 
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                  
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W', 
+    } 
+    protein ="" 
+    if len(seq)%3 == 0: 
+        for i in range(0, len(seq), 3): 
+            cod = seq[i:i + 3] 
+            protein += table[cod] 
+    return protein 
+def posiciones(codon):
+    codon = codon.upper()
+    nucleotidos = ['A','G','T','C']
+    split_codon = [i for i in codon]
+
+    # Primer check, tamaño (3)
+    if len(codon)!=3:
+        print('Introduce una secuencia múltiplo de 3 nucleótidos.')
+    
+    # Segundo check, los codones
+    for i in split_codon:
+        if i in nucleotidos:
+            pass
+        else:
+            print('Introduce nucleótidos válidos (carácter erróneo: {0})'.format(i))
+    
+    # Vamos a modificar las posiciones de los nucleótidos
+    else: 
+        translated_codon = translate(codon)
+        
+        codones = []
+        aas = []
+        for i in range(0,len(split_codon)):
+            split_codon = [i for i in codon]
+            nucleotidos = ['A','G','T','C']
+            nucleotidos.remove(split_codon[i])
+            columna_codones = []
+            columna_aas = []
+            for j in range(0,len(codon)):
+                split_codon[i] = nucleotidos[j]
+                columna_codones.append(''.join(split_codon))
+                columna_aas.append(translate(''.join(split_codon)))
+            codones.append(columna_codones)
+            aas.append(columna_aas)
+    
+    # Comparamos los aminoácidos con el aminoácido principal para obtener s y n
+    codon_aa = translate(codon)
+    sn = []
+    for i in aas:
+        s, n = 0, 0
+        for j in i:
+            if j == codon_aa:
+                s += 1
+            else:
+                n += 1
+        try:
+            sn.append([s/len(codon),n/len(codon)])
+        except ZeroDivisionError:
+            return None
+    
+    # Sumamos todo
+    s = 0
+    n = 0
+    for i in sn:
+        s += i[0]
+        n += i[1]
+    
+    n_posiciones = [s,n]
+    
+    for i in '.'*5:
+        print(i)
+        sleep(0.3)
+        
+    
+    print('Codón: ', codon)
+    print('Aminoácido: ', codon_aa)
+    sleep(0.5)
+    print('-'*20)
+    print('Posiciones cambiadas por codón: ')
+    print(codon[0],': ',codones[0])
+    print(codon[1],': ',codones[1])
+    print(codon[2],': ',codones[2])
+    sleep(0.5)
+    print('-'*20)
+    print('Aminoácidos: ')
+    print(aas[0])
+    print(aas[1])
+    print(aas[2])
+    sleep(0.5)
+    print('-'*20)
+    print('Resultados por nucleótido: ')
+    for i in range(0,len(codon)):
+        print('s{0} = '.format(codon[i]),sn[i][0])
+        print('n{0} = '.format(codon[i]),sn[i][1])
+        print('-'*5)
+    sleep(0.5)
+    print('-'*20)
+    print('Resultado codón')
+    print('s{0} = '.format(codon),n_posiciones[0])
+    print('n{0} = '.format(codon),n_posiciones[1])
+    
+    return n_posiciones
+
+print('Bienvenido a la calculadora de número de posiciones sinónimas y no sinónimas.')
+codon_input = input('Introduce un codón: ')
+posiciones(codon_input)
